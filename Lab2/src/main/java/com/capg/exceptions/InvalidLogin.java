7d8dcd120e146937e21d9be5ff4833d13fa63c26
ritiktomar <63736061+ritiktomar@users.noreply.github.com>
2020-04-30 23:16:45 +0530

    Add files via upload

---
+++ Lab2/src/main/java/com/capg/exceptions/InvalidLogin.java
@@ -0,0 +1,18 @@
+package com.capg.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value=HttpStatus.NOT_ACCEPTABLE,reason="Username or password is invalid")
+public class InvalidLogin extends RuntimeException {
+
+	
+	public InvalidLogin() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+}
